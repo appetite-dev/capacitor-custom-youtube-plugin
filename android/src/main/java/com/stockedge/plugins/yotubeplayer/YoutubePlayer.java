956806d33f050c046439444a21af2e2a17f35664
Sanjib Kumar Mandal <sanjib.mandal@stockedge.com>
2026-05-09 00:44:37 +0530

    Youtube player class updated with it's own xml file

--- android/src/main/java/com/stockedge/plugins/yotubeplayer/YoutubePlayer.java
+++ android/src/main/java/com/stockedge/plugins/yotubeplayer/YoutubePlayer.java
@@ -2,13 +2,22 @@ package com.stockedge.plugins.yotubeplayer;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.youtube.player.YouTubePlayerSupportFragmentX;
 
 public class YoutubePlayer extends AppCompatActivity {
-
     private static final String TAG = "YoutubePlayer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        YouTubePlayerSupportFragmentX youTubePlayerFragment =
+            YouTubePlayerSupportFragmentX.newInstance();
+
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.youtube_player_fragment, youTubePlayerFragment)
+            .commit();
     }
 }
